49f0ddb40d09f44c5ea3c95281fade64f3528f7e
Benjamin Fontaine <Benjamin77510be@gmail.com>
2025-08-06 14:56:53 +0200

    modification et ajout du caroussel et son style

--- Kasa/src/components/Carousel.jsx
+++ Kasa/src/components/Carousel.jsx
@@ -1,37 +1,55 @@
 import { useState } from "react";
 import "../scss/components/_carousel.scss"; // a modifier plus tard
+import leftArrow from "../assets/icones/Fleche-gauche.png";
+import rightArrow from "../assets/icones/Fleche-droite.png";
 
-export default function Carousel({ pictures }) {
-    const [currentIndex, setCurrentIndex] = useState(0);
+function SlideShow({ pictures }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const length = pictures.length;
 
-    const prevSlide = () => {
-        setCurrentIndex((prevIndex) =>
-        prevIndex === 0 ? pictures.length - 1 : prevIndex - 1
-        );
-    };
+  const goToPrevious = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? length - 1 : prevIndex - 1
+    );
+  };
 
-    const nextSlide = () => {
-        setCurrentIndex((prevIndex) => 
-        prevIndex === pictures.length - 1 ? 0 : prevIndex + 1
-        );
-    };
+  const goToNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === length - 1 ? 0 : prevIndex + 1
+    );
+  };
 
   return (
-      <div className="carousel">
-          <img
-          src={pictures[currentIndex]}
-          alt={`Logement image ${currentIndex + 1}`}
-          className="carousel-image"
-          />
-          {pictures.length > 1 && (
-              <>
-              <button className="carousel-prev" onClick={prevSlide}>‹</button>
-              <button className="carousel-next" onClick={nextSlide}>›</button>
-              </>
-          )}
-          <div className="carousel-counter">
-              {currentIndex + 1}/{pictures.length}
+    <div className="slideshow">
+      <img
+        src={pictures[currentIndex]}
+        alt={`Slide ${currentIndex + 1}`}
+        className="slideshow-image"
+      />
+
+      {length > 1 && (
+        <>
+          <button
+            className="nav left"
+            onClick={goToPrevious}
+            aria-label="Image précédente"
+          >
+            <img src={leftArrow} alt="Flèche gauche" />
+          </button>
+          <button
+            className="nav right"
+            onClick={goToNext}
+            aria-label="Image suivante"
+          >
+            <img src={rightArrow} alt="Flèche droite" />
+          </button>
+          <div className="counter">
+            {currentIndex + 1} / {length}
           </div>
-      </div>
+        </>
+      )}
+    </div>
   );
 }
+
+export default SlideShow;
